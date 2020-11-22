dd44bc74fa41477c594c063fd85b4562b8710493
Andrew McRae <amcrae@google.com>
2020-11-22 12:16:29 +1100

    config: Add interface. Run go fmt

--- config.go
+++ config.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Google LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,26 +15,33 @@
 package config
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
+
+// Interface for accessing configuration tokens.
+type Conf interface {
+	Has(string) bool
+	Get(string) []*Entry
+	GetArg(string) (string, error)
+}
 
 // Entry, one for each keyword.
 type Entry struct {
-    Keyword string
-    Filename string
-    Lineno int
-    Line string
-    Tokens []string
+	Keyword  string
+	Filename string
+	Lineno   int
+	Line     string
+	Tokens   []string
 }
 
 // The entries associated with a single section.
 // The default section is named 'global'.
 type Section struct {
-    m map[string][]*Entry
-    entries []*Entry
+	m       map[string][]*Entry
+	entries []*Entry
 }
 
 type Config struct {
@@ -43,22 +50,22 @@ type Config struct {
 
 const Global = "global"
 
-var delimiters map[rune]struct{} = map[rune]struct{}{ '=':struct{}{}, ',':struct{}{} }
+var delimiters map[rune]struct{} = map[rune]struct{}{'=': struct{}{}, ',': struct{}{}}
 
 func SetDelimiters(d string) {
-    delimiters = map[rune]struct{}{}
-    for _, r := range d {
-        delimiters[r] = struct{}{}
-    }
+	delimiters = map[rune]struct{}{}
+	for _, r := range d {
+		delimiters[r] = struct{}{}
+	}
 }
 
 func (c *Config) Merge(c1 *Config) {
-    for s, v := range c1.sections {
+	for s, v := range c1.sections {
 		sect := c.getSection(s, true)
 		for _, e := range v.entries {
 			sect.addEntry(e)
 		}
-    }
+	}
 }
 
 func (c *Config) Has(k string) bool {
@@ -92,45 +99,44 @@ func (config *Config) getSection(name string, create bool) *Section {
 	return s
 }
 
-
 func (c *Config) ParseFile(file string) error {
-    return c.parseOneFile(file)
+	return c.parseOneFile(file)
 }
 
 // Return strings not present in config.
 func (c *Config) Missing(strs []string) []string {
-    var missing []string
-    for _, s := range strs {
-        if _, ok := c.sections[Global].m[s]; !ok {
-            missing = append(missing, s)
-        }
-    }
-    return missing
+	var missing []string
+	for _, s := range strs {
+		if _, ok := c.sections[Global].m[s]; !ok {
+			missing = append(missing, s)
+		}
+	}
+	return missing
 }
 
 func (s *Section) Has(k string) bool {
-    _, ok := s.m[k]
+	_, ok := s.m[k]
 	return ok
 }
 
 func (s *Section) Get(k string) []*Entry {
-    if v, ok := s.m[k]; ok {
-        return v
-    }
-    return []*Entry{}
+	if v, ok := s.m[k]; ok {
+		return v
+	}
+	return []*Entry{}
 }
 
 func (s *Section) GetArg(k string) (string, error) {
-    if v, ok := s.m[k]; ok {
-        if len(v) != 1 {
-            return "", fmt.Errorf("Illegal config for '%s'", k)
-        }
-        if len(v[0].Tokens) != 1 {
-            return "", fmt.Errorf("Illegal arguments for '%s'", k)
-        }
-        return v[0].Tokens[0], nil
-    }
-    return "", fmt.Errorf("Missing keyword: %s", k)
+	if v, ok := s.m[k]; ok {
+		if len(v) != 1 {
+			return "", fmt.Errorf("Illegal config for '%s'", k)
+		}
+		if len(v[0].Tokens) != 1 {
+			return "", fmt.Errorf("Illegal arguments for '%s'", k)
+		}
+		return v[0].Tokens[0], nil
+	}
+	return "", fmt.Errorf("Missing keyword: %s", k)
 }
 
 func (s *Section) GetEntries() []*Entry {
@@ -139,33 +145,33 @@ func (s *Section) GetEntries() []*Entry {
 
 func ParseFiles(optional bool, files []string) (*Config, error) {
 	config := newConfig()
-    for _, f := range files {
-        if err := config.parseOneFile(f); err != nil {
-            if !optional {
-                return config, fmt.Errorf("%s: %v", f, err)
-            }
-        }
-    }
-    return config, nil
+	for _, f := range files {
+		if err := config.parseOneFile(f); err != nil {
+			if !optional {
+				return config, fmt.Errorf("%s: %v", f, err)
+			}
+		}
+	}
+	return config, nil
 }
 
 func ParseFile(file string) (*Config, error) {
 	config := newConfig()
-    return config, config.parseOneFile(file)
+	return config, config.parseOneFile(file)
 }
 
 func (config *Config) parseOneFile(file string) error {
-    f, err := os.Open(file)
-    if err != nil {
-        return err
-    }
-    defer f.Close()
-    return config.parse(file, bufio.NewReader(f))
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return config.parse(file, bufio.NewReader(f))
 }
 
 func ParseString(s string) (*Config, error) {
 	config := newConfig()
-    return config, config.parse("internal", bufio.NewReader(strings.NewReader(s)))
+	return config, config.parse("internal", bufio.NewReader(strings.NewReader(s)))
 }
 
 // newConfig creates a new Config structure.
@@ -186,42 +192,42 @@ func newConfig() *Config {
 // Duplicate keywords are silently overwritten.
 func (config *Config) parse(source string, r *bufio.Reader) error {
 	sect := config.sections[Global]
-    lineno := 0
-    scanner := bufio.NewScanner(r)
-    for scanner.Scan() {
-        lineno++
-        l := strings.TrimSpace(scanner.Text())
-        if len(l) == 0 || l[0:1] == "#" {
-            continue
-        }
+	lineno := 0
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lineno++
+		l := strings.TrimSpace(scanner.Text())
+		if len(l) == 0 || l[0:1] == "#" {
+			continue
+		}
 		ln := len(l)
 		// Check for new section.
 		if ln > 2 && l[0] == '[' && l[ln-1] == ']' {
 			sect = config.getSection(l[1:ln-1], true)
 			continue
 		}
-        tok := strings.FieldsFunc(l, checkDelimiter)
-        if len(tok) == 0 {
-            continue
-        }
-        sect.addEntry(&Entry{tok[0], source, lineno, l, tok[1:]})
-    }
-    if scanner.Err() != nil {
-        return fmt.Errorf("%s: line %d: %v", source, lineno, scanner.Err())
-    }
-    return nil
+		tok := strings.FieldsFunc(l, checkDelimiter)
+		if len(tok) == 0 {
+			continue
+		}
+		sect.addEntry(&Entry{tok[0], source, lineno, l, tok[1:]})
+	}
+	if scanner.Err() != nil {
+		return fmt.Errorf("%s: line %d: %v", source, lineno, scanner.Err())
+	}
+	return nil
 }
 
 func (sect *Section) addEntry(v *Entry) {
-    sect.entries = append(sect.entries, v)
-    entry, ok := sect.m[v.Keyword]
-    if !ok {
-        entry = []*Entry{}
-    }
-    sect.m[v.Keyword] = append(entry, v)
+	sect.entries = append(sect.entries, v)
+	entry, ok := sect.m[v.Keyword]
+	if !ok {
+		entry = []*Entry{}
+	}
+	sect.m[v.Keyword] = append(entry, v)
 }
 
 func checkDelimiter(r rune) bool {
-    _, found := delimiters[r]
-    return found
+	_, found := delimiters[r]
+	return found
 }
--- config_test.go
+++ config_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Google LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,98 +15,98 @@
 package config_test
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/aamcrae/config"
-    "path/filepath"
-//    "reflect"
+	"github.com/aamcrae/config"
+	"path/filepath"
+	//    "reflect"
 )
 
 func TestString(t *testing.T) {
-    s := `#
+	s := `#
 # Comment line 1
 
 keyword=test
 key2
 key3=data1,data2,data3
 `
-    c, err := config.ParseString(s)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if len(c.GetSection(config.Global).GetEntries()) != 3 {
-        t.Fatalf("Wrong number of config entries")
-    }
-    val := c.Get("keyword")
-    if len(val) != 1 {
-        t.Fatalf("wrong number of entries for keyword: %v", c)
-    }
-    if val[0].Lineno != 4 {
-        t.Fatalf("wrong line number for 'keyword'")
-    }
-    if len(val[0].Tokens) != 1 {
-        t.Fatalf("Wrong number of tokens for 'keyword'")
-    }
-    if val[0].Tokens[0] != "test" {
-        t.Fatalf("Wrong token value for 'keyword'")
-    }
-    val = c.Get("key2")
-    if len(val) != 1 {
-        t.Fatalf("wrong number of entries for 'key2'")
-    }
-    if val[0].Lineno != 5 {
-        t.Fatalf("wrong line number for 'key2'")
-    }
-    if len(val[0].Tokens) != 0 {
-        t.Fatalf("Wrong number of tokens for 'key2'")
-    }
-    val = c.Get("key3")
-    if len(val) != 1 {
-        t.Fatalf("wrong number of entries for 'key3'")
-    }
-    if val[0].Lineno != 6 {
-        t.Fatalf("wrong line number for 'key3'")
-    }
-    if len(val[0].Tokens) != 3 {
-        t.Fatalf("Wrong number of tokens for 'key3'")
-    }
+	c, err := config.ParseString(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(c.GetSection(config.Global).GetEntries()) != 3 {
+		t.Fatalf("Wrong number of config entries")
+	}
+	val := c.Get("keyword")
+	if len(val) != 1 {
+		t.Fatalf("wrong number of entries for keyword: %v", c)
+	}
+	if val[0].Lineno != 4 {
+		t.Fatalf("wrong line number for 'keyword'")
+	}
+	if len(val[0].Tokens) != 1 {
+		t.Fatalf("Wrong number of tokens for 'keyword'")
+	}
+	if val[0].Tokens[0] != "test" {
+		t.Fatalf("Wrong token value for 'keyword'")
+	}
+	val = c.Get("key2")
+	if len(val) != 1 {
+		t.Fatalf("wrong number of entries for 'key2'")
+	}
+	if val[0].Lineno != 5 {
+		t.Fatalf("wrong line number for 'key2'")
+	}
+	if len(val[0].Tokens) != 0 {
+		t.Fatalf("Wrong number of tokens for 'key2'")
+	}
+	val = c.Get("key3")
+	if len(val) != 1 {
+		t.Fatalf("wrong number of entries for 'key3'")
+	}
+	if val[0].Lineno != 6 {
+		t.Fatalf("wrong line number for 'key3'")
+	}
+	if len(val[0].Tokens) != 3 {
+		t.Fatalf("Wrong number of tokens for 'key3'")
+	}
 }
 
 func TestFile(t *testing.T) {
-    c, err := config.ParseFile(filepath.Join("testdata", "f1"))
-    if err != nil {
-        t.Fatalf("File read for f1 failed: %v", err)
-    }
+	c, err := config.ParseFile(filepath.Join("testdata", "f1"))
+	if err != nil {
+		t.Fatalf("File read for f1 failed: %v", err)
+	}
 	l := len(c.GetSection(config.Global).GetEntries())
-    if l != 3 {
-        t.Fatalf("TestFile: wrong number of entries: %d", l)
-    }
+	if l != 3 {
+		t.Fatalf("TestFile: wrong number of entries: %d", l)
+	}
 	l = len(c.GetSection("section1").GetEntries())
-    if l != 2 {
-        t.Fatalf("TestFile: wrong number of entries for 'section1': %d", l)
-    }
-    val := c.Get("key1")
-    if len(val) != 1 {
-        t.Fatalf("wrong number of entries for 'key1'")
-    }
+	if l != 2 {
+		t.Fatalf("TestFile: wrong number of entries for 'section1': %d", l)
+	}
+	val := c.Get("key1")
+	if len(val) != 1 {
+		t.Fatalf("wrong number of entries for 'key1'")
+	}
 	exp := [...]string{"1", "2", "3", "4"}
-    if len(val[0].Tokens) != len(exp) {
-        t.Fatalf("TestFile: Wrong number of tokens for 'key1'")
-    }
+	if len(val[0].Tokens) != len(exp) {
+		t.Fatalf("TestFile: Wrong number of tokens for 'key1'")
+	}
 	for i, v := range exp {
 		if val[0].Tokens[i] != v {
 			t.Fatalf("TestFile: Wrong token %d for 'key1', expected %s, got %s", i, v, val[0].Tokens[i])
 		}
 	}
 	s1 := c.GetSection("section1")
-    val = s1.Get("key1")
-    if len(val) != 1 {
-        t.Fatalf("wrong number of entries for 'section1' 'key1'")
-    }
+	val = s1.Get("key1")
+	if len(val) != 1 {
+		t.Fatalf("wrong number of entries for 'section1' 'key1'")
+	}
 	exp1 := [...]string{"x", "y", "z"}
-    if len(val[0].Tokens) != len(exp1) {
-        t.Fatalf("TestFile: Wrong number of tokens for 'section1' 'key1'")
-    }
+	if len(val[0].Tokens) != len(exp1) {
+		t.Fatalf("TestFile: Wrong number of tokens for 'section1' 'key1'")
+	}
 	for i, v := range exp1 {
 		if val[0].Tokens[i] != v {
 			t.Fatalf("TestFile: Wrong token %d for 'section1' 'key1', expected %s, got %s", i, v, val[0].Tokens[i])
@@ -115,74 +115,74 @@ func TestFile(t *testing.T) {
 }
 
 func TestMultiFile(t *testing.T) {
-    p1 := filepath.Join("testdata", "f1")
-    p2 := filepath.Join("testdata", "f2")
-    c, err := config.ParseFiles(true, []string{p1, p2})
-    if err != nil {
-        t.Fatalf("File read for f1/f2 failed: %v", err)
-    }
+	p1 := filepath.Join("testdata", "f1")
+	p2 := filepath.Join("testdata", "f2")
+	c, err := config.ParseFiles(true, []string{p1, p2})
+	if err != nil {
+		t.Fatalf("File read for f1/f2 failed: %v", err)
+	}
 	l := len(c.GetSection(config.Global).GetEntries())
-    if l != 6 {
-        t.Fatalf("TestFiles: wrong number of entries: %d", l)
-    }
-    val := c.Get("key3")
-    if len(val) != 2 {
-        t.Fatalf("wrong number of entries for 'key1'")
-    }
-    if len(val[0].Tokens) != 1 || len(val[1].Tokens) != 1 {
-        t.Fatalf("TestFiles: Wrong number of tokens for 'key3'")
-    }
-    if val[0].Tokens[0] != "abc" || val[1].Tokens[0] != "xyz" {
-        t.Fatalf("TestFiles: Incorrect values for 'key3'")
-    }
+	if l != 6 {
+		t.Fatalf("TestFiles: wrong number of entries: %d", l)
+	}
+	val := c.Get("key3")
+	if len(val) != 2 {
+		t.Fatalf("wrong number of entries for 'key1'")
+	}
+	if len(val[0].Tokens) != 1 || len(val[1].Tokens) != 1 {
+		t.Fatalf("TestFiles: Wrong number of tokens for 'key3'")
+	}
+	if val[0].Tokens[0] != "abc" || val[1].Tokens[0] != "xyz" {
+		t.Fatalf("TestFiles: Incorrect values for 'key3'")
+	}
 }
 
 func TestApi(t *testing.T) {
-    c, err := config.ParseFile(filepath.Join("testdata", "f1"))
-    if err != nil {
-        t.Fatalf("TestApi: File read for f1 failed: %v", err)
-    }
-    v := c.Get("key1")
-    if len(v) != 1 {
-        t.Fatalf("wrong number of entries for 'key1'")
-    }
-    if len(v[0].Tokens) != 4 {
-        t.Fatalf("TestApi: Get returns wrong number of tokens: %v", v[0].Tokens)
-    }
-    exp := []string{"1", "2", "3", "4"}
-    for i, tok := range exp {
-        if v[0].Tokens[i] != tok {
-            t.Fatalf("TestApi: Get returns wrong tokens : %v", v[0].Tokens)
-        }
-    }
-    strs := c.Missing([]string{"key2", "key1", "key5"})
-    if len(strs) != 1 {
-        t.Fatalf("TestApi: Missing returns wrong number: %v", strs)
-    }
-    if strs[0] != "key5" {
-        t.Fatalf("TestApi: Missing returns wrong string: %v", strs)
-    }
+	c, err := config.ParseFile(filepath.Join("testdata", "f1"))
+	if err != nil {
+		t.Fatalf("TestApi: File read for f1 failed: %v", err)
+	}
+	v := c.Get("key1")
+	if len(v) != 1 {
+		t.Fatalf("wrong number of entries for 'key1'")
+	}
+	if len(v[0].Tokens) != 4 {
+		t.Fatalf("TestApi: Get returns wrong number of tokens: %v", v[0].Tokens)
+	}
+	exp := []string{"1", "2", "3", "4"}
+	for i, tok := range exp {
+		if v[0].Tokens[i] != tok {
+			t.Fatalf("TestApi: Get returns wrong tokens : %v", v[0].Tokens)
+		}
+	}
+	strs := c.Missing([]string{"key2", "key1", "key5"})
+	if len(strs) != 1 {
+		t.Fatalf("TestApi: Missing returns wrong number: %v", strs)
+	}
+	if strs[0] != "key5" {
+		t.Fatalf("TestApi: Missing returns wrong string: %v", strs)
+	}
 }
 
 func TestMerge(t *testing.T) {
-    c, err := config.ParseFile(filepath.Join("testdata", "f1"))
-    if err != nil {
-        t.Fatalf("TestMerge: File read for f1 failed: %v", err)
-    }
-    c1, err := config.ParseFile(filepath.Join("testdata", "f2"))
-    if err != nil {
-        t.Fatalf("TestMerge: File read for f2 failed: %v", err)
-    }
-    p1 := filepath.Join("testdata", "f1")
-    p2 := filepath.Join("testdata", "f2")
-    comp, err := config.ParseFiles(true, []string{p1, p2})
-    if err != nil {
-        t.Fatalf("TestMerge: File read for f1/f2 failed: %v", err)
-    }
-    c.Merge(c1)
-    // A bit tricky to compare, since the values are pointers.
+	c, err := config.ParseFile(filepath.Join("testdata", "f1"))
+	if err != nil {
+		t.Fatalf("TestMerge: File read for f1 failed: %v", err)
+	}
+	c1, err := config.ParseFile(filepath.Join("testdata", "f2"))
+	if err != nil {
+		t.Fatalf("TestMerge: File read for f2 failed: %v", err)
+	}
+	p1 := filepath.Join("testdata", "f1")
+	p2 := filepath.Join("testdata", "f2")
+	comp, err := config.ParseFiles(true, []string{p1, p2})
+	if err != nil {
+		t.Fatalf("TestMerge: File read for f1/f2 failed: %v", err)
+	}
+	c.Merge(c1)
+	// A bit tricky to compare, since the values are pointers.
 	l := len(c.GetSection(config.Global).GetEntries())
-    if l != len(comp.GetSection(config.Global).GetEntries()) {
-        t.Fatalf("TestMerge: lengths are different: %v %v", c1, comp)
-    }
+	if l != len(comp.GetSection(config.Global).GetEntries()) {
+		t.Fatalf("TestMerge: lengths are different: %v %v", c1, comp)
+	}
 }
